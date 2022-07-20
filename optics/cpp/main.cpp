a43c4232218606d649a3e6e9edd376b62506a02c
Luca <unluckyluke@DESKTOP-49O5A5K.localdomain>
2022-07-20 22:36:49 +0200

    added opencv for c++

--- optics/cpp/main.cpp
+++ optics/cpp/main.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 //#include <pybind11/pybind11.h>
 
-//#include <opencv2/imgcodecs.hpp>
+#include <opencv4/opencv2/highgui/highgui.hpp>
+#include <opencv4/opencv2/core/core.hpp>
 
 using namespace std;
 
 int main(){
 
+    cv::Mat frame;
+    frame = cv::imread("example.jpeg");
+
+    cout << "Test" << endl;
+
+    if(frame.empty()) cout << "Image not successfully loaded! " << endl;
+
+    cv::imshow("Image", frame);
+
     cout << "Test in Optics cpp!" << endl;
+
+    cv::waitKey(0);
 
     return 0;
 }
