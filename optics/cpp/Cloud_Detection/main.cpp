352fa931bb6259fa7a7670ded21caae6abafcde8
unluckyLuke <unluckyluke@gmail.com>
2022-07-25 19:12:46 +0200

    live cloud Detection

--- optics/cpp/Cloud_Detection/main.cpp
+++ optics/cpp/Cloud_Detection/main.cpp
@@ -3,6 +3,7 @@
 // Loading opencv libraries:
 #include <opencv4/opencv2/highgui.hpp>
 #include <opencv4/opencv2/imgproc/imgproc.hpp>
+#include <opencv2/opencv.hpp>
 
 #include <iostream>
 #include <vector>
@@ -21,63 +22,63 @@ cv::Scalar whiteHigh = cv::Scalar(180, 155, 255);
 
 int main(){
 
-    // Loading the Image:
-    cv::Mat target = cv::imread("clouds.jpg");
-    cout << "Target Size: " << target.size() << endl;
-    cv::resize(target, target, cv::Size(target.size().width / 2, target.size().height / 2), cv::INTER_LINEAR);
-    cout << "Target Size: " << target.size() << endl;
+    // Setup Camera:
+    cv::Mat frame;
+    cv::VideoCapture webcam(0);
 
-    cv::Mat original = cv::imread("clouds.jpg");
-    cv::resize(original, original, cv::Size(original.size().width / 2, original.size().height / 2), cv::INTER_LINEAR);
-    cv::imshow("Original", original);
-
-    // Create second Picture:
-    cv::Mat background;
-    target.copyTo(background);
-
-    // Convert the Target Image from BGR to HSV:
-    cv::cvtColor(target, target, cv::COLOR_BGR2HSV);
-
-    // Create an Image to see the white spaces in the original Target:
-    cv::Mat mask;
-    cv::inRange(target, whiteLow, whiteHigh, mask);
-    cv::imshow("white", mask);
-
-    // Going trough every Pixel from the Original and from mask:
-    //cv::cvtColor(mask, mask, cv::COLOR_HSV2RGB);
-
-    cout << "Mask Size Width: " << mask.size().width << "   Mask Size Height: " << mask.size().height <<endl;
-    cout << "Background Size: " << background.size() << endl;
-
-    background.copyTo(original);
-
-    // Contours in original:
-    std::vector<std::vector<cv::Point>> contours;
-    cv::findContours(mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-    cv::drawContours(original, contours, -1, (0, 255, 0), 3);
-    cv::imshow("Contours", original);
-
-    // Find Pixels with pieces of Clouds:
-    cv::cvtColor(background, background, cv::COLOR_BGR2RGB);
-    for(int i = 0; i < background.rows; i++){
-
-        for(int j = 0; j < background.cols; j++){
-
-            // Check if the Pixel is from a piece of a Cloud:
-            if((int)mask.at<uchar>((int)i, (int)j) == 255){ // There is Just one Value in every mask Pixel -> mask is Grayscale:
-
-                background.at<cv::Vec3b>((int)i, (int)j)[0] += 100;
-                background.at<cv::Vec3b>((int)i, (int)j)[1] += 0;
-                background.at<cv::Vec3b>((int)i, (int)j)[2] += 0;
-            }
-        }
+    // Check if VideoCapture is working:
+    if(!webcam.isOpened()){ 
+        
+        cout << "Video Capture failed!" << endl;
+        return -1;
     }
 
-    cv::cvtColor(background, background, cv::COLOR_RGB2BGR);
+    // Loop to compute everything:
+    while(webcam.read(frame)){
 
-    cv::imshow("Original Boosted", background);
+        // Resize the Frame:
+        cv::resize(frame, frame, cv::Size(), 0.5, 0.5);
 
-    cv::waitKey(0);
+        /*
+        // Create copy of the Frame:
+        cv::Mat frameHSV;
+        cv::Mat frameBoosted;
+        frame.copyTo(frameHSV);
+        frame.copyTo(frameBoosted);
+
+        // Create HSV Color of the Frame:
+        cv::cvtColor(frameHSV, frameHSV, cv::COLOR_BGR2HSV);
+
+        // Create an Image to see the white spaces in the original Target:
+        cv::Mat mask;
+        cv::inRange(frameHSV, whiteLow, whiteHigh, mask);
+        cv::imshow("Mask", mask);
+
+        // Going trough every Pixel from the Original and from mask:
+        // Find Pixels with pieces of Clouds:
+        for(int i = 0; i < mask.rows; i++){
+
+            for(int j = 0; j < mask.cols; j++){
+
+                // Check if the Pixel is from a piece of a Cloud:
+                if((int)mask.at<uchar>((int)i, (int)j) == 255){ // There is Just one Value in every mask Pixel -> mask is Grayscale:
+
+                    frameBoosted.at<cv::Vec3b>((int)i, (int)j)[0] += 100;
+                    frameBoosted.at<cv::Vec3b>((int)i, (int)j)[1] += 0;
+                    frameBoosted.at<cv::Vec3b>((int)i, (int)j)[2] += 0;
+                }
+            }
+        }
+        */
+
+       cout << "Test" << endl;
+
+        cv::imshow("Original", frame);
+        //cv::imshow("Boosted", frameBoosted);
+
+        // Escape the Program:
+        if(cv::waitKey() == 'q') break;
+    }
 
     return 0;
 }
