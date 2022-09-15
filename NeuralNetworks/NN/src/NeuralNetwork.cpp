45b76482f6b8056fa05a79c54daff31c31cafa06
unluckyLuke <luca.rubeni@t-online.de>
2022-09-15 22:32:01 +0200

    started NN class

--- NeuralNetworks/NN/src/NeuralNetwork.cpp
+++ NeuralNetworks/NN/src/NeuralNetwork.cpp
@@ -3,5 +3,3 @@
 Neuron::Neuron (int connections) : connectionWeights(connections, 0.0){}
 
 Layer::Layer (int n) : neurons(n, Neuron(0)){}
-
-NeuralNetwork::NeuralNetwork (int inputNeurons, int nHLayers, int outputNeurons) : inputLayer(inputNeurons), 
