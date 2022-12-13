b8a6887b9c913a4562c298e4bb5f559f9107d206
unluckyLuke <luca.rubeni@t-online.de>
2022-12-13 20:32:44 +0100

    not working at all

--- NeuralNetworks/NN/include/NeuralNetwork.hpp
+++ NeuralNetworks/NN/include/NeuralNetwork.hpp
@@ -1,2 +1,26 @@
 #include <vector>
+#include <iostream>
 
+using std::vector;
+using std::cout;
+using std::endl;
+
+#pragma once
+
+class Node{
+public:
+    Node(double v);
+    double activation();
+    double weightNo(int n);
+private:
+    double value;
+    vector<double> weights;
+};
+
+class Net{
+public:
+    Net(int m, int n);
+
+private:
+    vector<vector<Node>> net;
+};
--- NeuralNetworks/NN/main.cpp
+++ NeuralNetworks/NN/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <math.h>
 
+#include "NeuralNetwork.hpp"
+
+using std::cout;
+using std::endl;
+
 int main(){
+
+    Net myNet(4, 5);
 
     return 0;
 }
